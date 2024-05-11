cae12ef925d8b6933339c15194b09dc96203f195
HiiipowerHiii <163337839+HiiipowerHiii@users.noreply.github.com>
2024-05-12 02:32:07 +0300

    add optimization

--- logger.js
+++ logger.js
@@ -2,33 +2,41 @@ const fs = require('fs');
 const path = require('path');
 const winston = require('winston');
 require('dotenv').config();
+
 const logDir = 'logs';
 if (!fs.existsSync(logDir)) {
   fs.mkdirSync(logDir);
 }
-const envLoglevel = process.env.LOG_LEVEL || 'info';
-const logFilename = process.env.LOG_FILE || 'application.log';
+
+const defaultLogLevel = 'info';
+const defaultLogFilename = 'application.log';
+
+const envLoglevel = process.env.LOG_LEVEL || defaultLogLevel;
+const logFilename = process.env.LOG_FILE || defaultLogFilename;
+
+const customFormat = winston.format.combine(
+  winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+  winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+);
+
 const logger = winston.createLogger({
   level: envLoglevel,
-  format: winston.format.combine(
-    winston.format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss'
-    }),
-    winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
-  ),
+  format: customFormat,
   transports: [
     new winston.transports.Console({
       format: winston.format.simple(),
-    }),
+    }), 
     new winston.transports.File({ filename: path.join(logDir, logFilename) })
   ]
 });
+
 function logMessage(level, message) {
   logger.log({
-    level: level,
-    message: message
+    level,
+    message
   });
 }
+
 module.exports = {
   logMessage
 };
